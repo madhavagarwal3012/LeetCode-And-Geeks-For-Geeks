90b0b0cf5e446d5c018795f31c200cc839eea25a
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2023-06-21 04:54:06 +0530

    Time: 4 ms (94.69%), Space: 44 MB (14.66%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,12 +1,5 @@
 class Solution {
     public int romanToInt(String s) {
-        // int I=1;
-        // int V=5;
-        // int X=10;
-        // int L=50;
-        // int C=100;
-        // int D=500;
-        // int M=1000;
         
         int start=0;
         int sum=0;
@@ -14,6 +7,7 @@ class Solution {
         int final_sum=0;
         int end=s.length();
         while(start<end){
+            
             if(s.charAt(start)=='I'){
                 sum=1;
             }
@@ -43,24 +37,15 @@ class Solution {
             }
             if(previous_sum>=sum ||sum==final_sum){
                 final_sum=sum+final_sum;
-                System.out.println("final_sum fs1:"+final_sum);
             }
             else if(sum>previous_sum){
-                System.out.println("sum :"+sum);
-                System.out.println("previous_sum :"+previous_sum);
-                System.out.println("final_sum fs2 :"+final_sum);
-                
                 sum=sum-previous_sum;
                 final_sum=final_sum-previous_sum;
                 final_sum=sum+final_sum;
             }
-            System.out.println("start :"+start);
-            System.out.println("sum :"+sum);
             start++;
-            System.out.println("previous_sum :"+previous_sum);
             previous_sum=sum;
         }
-        
         return final_sum;
     }
 }
