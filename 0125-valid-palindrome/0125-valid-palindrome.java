6cf70b94387233964c4b513e64a9877c81166649
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2023-06-25 00:41:37 +0530

    Time: 598 ms (5.03%), Space: 45.4 MB (6.29%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -2,38 +2,37 @@ class Solution {
     public static String Reversed_String(String str) {
 		String reversed_string="";
 		int end= str.length()-1;
-		    while(end>=0){
-                if((int)(str.charAt(end))>=48 &&(int)(str.charAt(end))<=57){
-		    	    reversed_string+=str.charAt(end);
-		            end--;
-		        }
-                else if((int)(str.charAt(end))>=65 && (int)(str.charAt(end))<=90){
-		    	    reversed_string+=str.charAt(end);
-		            end--; 
-                }
-                else if((int)(str.charAt(end))>=97 && (int)(str.charAt(end))<=122){
-		    	    reversed_string+=str.charAt(end);
-		            end--; 
-                }
-                else{
-                    end--;
-                    continue;
-                }
-                
+		while(end>=0){
+            if((int)(str.charAt(end))>=48 &&(int)(str.charAt(end))<=57){
+                reversed_string+=str.charAt(end);
+		        end--;
 		    }
-		    return reversed_string;
+            else if((int)(str.charAt(end))>=65 && (int)(str.charAt(end))<=90){
+		    	reversed_string+=str.charAt(end);
+		        end--; 
+            }
+            else if((int)(str.charAt(end))>=97 && (int)(str.charAt(end))<=122){
+                reversed_string+=str.charAt(end);
+		        end--; 
+            }
+            else{
+                end--;
+                continue;
+            }     
+		}
+		return reversed_string;
 	}
     public boolean isPalindrome(String str) {
         String rs=Reversed_String(str.toLowerCase());
         str=Reversed_String(rs);
-        if(rs.length()==0){
-            return true;
-        }
 		int start=0;
 		int end=str.length();
 		boolean palindrome=false;
+        
+        if(rs.length()==0){
+            return true;
+        }
 		while(start<end) {
-			
 			if(rs.charAt(start)==str.charAt(start)) {
 				palindrome=true;
 			}
