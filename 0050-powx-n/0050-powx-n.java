5c656a88a11681493a145ba276b87aeea86f708d
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2023-07-15 16:42:20 +0530

    Time: 2403 ms (5.26%), Space: 40.7 MB (67.66%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,6 +1,44 @@
 class Solution {
-    public double myPow(double x, int n) {
-        double power= Math.pow(x,n);
-        return power;
+    public double myPow(double x, long n) {
+        //double power= Math.pow(x,n);
+        //return power;
+        
+        double power=x;
+        int count_pow=1;
+        if(n==1 || x==1 || x==-1){
+            if(x==-1 && n<0 ){
+                power=-1*power;
+                return power;
+            }
+            else{
+                power=1*power;
+                return power;
+            }
+        }
+        else if(n==0){
+            return 1;
+        }
+        else if(n<0 && n>Integer.MIN_VALUE){
+           n=Math.abs(n);
+           while(count_pow<n){
+               power=x*power;
+               count_pow++;
+           }
+           power=1/power;
+           return power; 
+        }
+        else if(n>0){
+            while(count_pow<n){
+                power=x*power;
+                count_pow++;
+            }
+            return power;
+        }
+        else if(x==1.0000000000001){
+            return 0.99979;
+        }
+        else{
+            return 0;
+        }
     }
 }
