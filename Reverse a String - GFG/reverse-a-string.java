6cccc8c144115bbf259a84ad4a44537b6b162a86
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2023-07-16 16:39:08 +0530

    Added solution - LeetHub

---
+++ Reverse a String - GFG/reverse-a-string.java
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Driver
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- >0)
+        {
+            String str = read.readLine();
+            System.out.println(new Reverse().reverseWord(str));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Reverse
+{
+    // Complete the function
+    // str: input string
+    public static String reverseWord(String str)
+    {
+        String reversed_string="";
+		int end= str.length()-1;
+		    while(end>=0){
+		    	reversed_string+=str.charAt(end);
+		        end--;
+		    }
+		        return reversed_string;
+    }
+}
