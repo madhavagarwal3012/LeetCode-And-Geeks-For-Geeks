dc9e33d96f6d806433786ed13431b2cc5c6ead06
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2023-08-15 15:44:57 +0530

    Time: 6 ms (9.21%), Space: 41.1 MB (11.92%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.java
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -1,6 +1,24 @@
 class Solution {
-    public String toLowerCase(String s) {
-        
-        return s.toLowerCase();
+    public String toLowerCase(String str) {
+        if(str.length()==1){
+            return str;
+        }
+        else{
+            String lower="";
+            int i=0;
+            
+            while(i<str.length()){
+                int ascii= str.charAt(i);
+                if(ascii>=65 && ascii<=90 ){
+                  lower+=(char)(ascii+32);
+                }
+                else{
+                   lower+=str.charAt(i);
+                }
+                  
+                i++;
+            }
+            return lower;
+        }
     }
 }
