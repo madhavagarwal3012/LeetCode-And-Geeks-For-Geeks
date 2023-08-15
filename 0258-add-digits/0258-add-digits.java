4d8cd5df3fa17a1d4bb2e1cd4cf531cedf78a555
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2023-08-15 13:52:17 +0530

    Time: 2 ms (5.67%), Space: 39.3 MB (93.76%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int addDigits(int n) {
+        if(n>9){
+            String str= Integer.toString(n);
+            int i=1;
+            int sum=Character.getNumericValue(str.charAt(0));
+            while(i<str.length()){
+                sum=sum+Character.getNumericValue(str.charAt(i));
+                i++;
+            }
+            if(sum>9){
+                str= Integer.toString(sum);
+                i=1;
+                int final_sum=Character.getNumericValue(str.charAt(0));
+                while(i<str.length()){
+                    final_sum=final_sum+Character.getNumericValue(str.charAt(i));
+                    i++;
+                }
+                if(final_sum>9){
+                   str= Integer.toString(final_sum);
+                    i=1;
+                    final_sum=Character.getNumericValue(str.charAt(0));
+                    while(i<str.length()){
+                        final_sum=final_sum+Character.getNumericValue(str.charAt(i));
+                        i++;
+                    } 
+                }
+                return final_sum;
+            }
+            else{
+                return sum;
+            }
+            
+        }
+        else{
+            return n;
+        }
+    }
+}
