e1cd5ea4c84b0a9774b2601d90127ae4899213ab
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2023-08-15 13:28:47 +0530

    Time: 1898 ms (8.60%), Space: 39.2 MB (96.14%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n==1 || n==2){
+            return true;
+        }
+        else{
+            if(n%2==0){
+                int i=2;
+                int power_oftwo=2;
+                while(i<=n/2){
+                    power_oftwo=2*power_oftwo;
+                    if(power_oftwo==n){
+                        return true;
+                    }
+                    i++;
+                }
+            }
+            return false;
+        }
+    }
+}
