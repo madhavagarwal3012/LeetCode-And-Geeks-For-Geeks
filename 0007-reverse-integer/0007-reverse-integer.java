11b8dc0723b7f25a6a04642be894973c6f0ad444
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2023-08-27 00:59:18 +0530

    Time: 10 ms (5.48%), Space: 40.9 MB (6.01%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,30 @@
+class Solution {
+    public static String Reversed_String(String str) {
+        String reversed_string = "";
+        int end = str.length() - 1;
+        while (end >= 0) {
+            reversed_string += str.charAt(end);
+            end--;
+        }
+        return reversed_string;
+    }
+    public int reverse(int x) {
+        if(x > Integer.MAX_VALUE || x < Integer.MIN_VALUE){
+            return 0;
+        }
+        String str=Integer.toString(x);
+        if(Character.getNumericValue(str.charAt(0))<0){
+            x=Math.abs(x);
+            str=Integer.toString(x);
+            str+="-";
+        }
+        str=Reversed_String(str);
+        
+        try {
+        x = Integer.parseInt(str);
+    } catch (NumberFormatException e) {
+        return 0; // Handle the case when the reversed string is out of valid integer range
+    }
+        return x;
+    }
+}
