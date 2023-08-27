86c3259ffbc42e11e99599aec782f62ec8af474b
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2023-08-27 18:34:38 +0530

    Time: 1 ms (93.62%), Space: 40 MB (27.21%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,29 +1,37 @@
 class Solution {
     public int addDigits(int n) {
-        if (n <= 9) {
-            return n;
-        }
-
-        String str = Integer.toString(n);
-        int sum = Character.getNumericValue(str.charAt(0));
-
-        int i = 1;
-        while (i < str.length()) {
-            sum = sum + Character.getNumericValue(str.charAt(i));
-            i++;
-        }
-
-        while (sum > 9) {
-            str = Integer.toString(sum);
-            sum = Character.getNumericValue(str.charAt(0));
-
-            i = 1;
-            while (i < str.length()) {
-                sum = sum + Character.getNumericValue(str.charAt(i));
+        if(n>9){
+            String str= Integer.toString(n);
+            int i=1;
+            int sum=Character.getNumericValue(str.charAt(0));
+            while(i<str.length()){
+                sum=sum+Character.getNumericValue(str.charAt(i));
                 i++;
             }
+            if(sum>9){
+                str= Integer.toString(sum);
+                i=1;
+                int final_sum=Character.getNumericValue(str.charAt(0));
+                while(i<str.length()){
+                    final_sum=final_sum+Character.getNumericValue(str.charAt(i));
+                    i++;
+                }
+                if(final_sum>9){
+                   str= Integer.toString(final_sum);
+                    i=1;
+                    final_sum=Character.getNumericValue(str.charAt(0));
+                    while(i<str.length()){
+                        final_sum=final_sum+Character.getNumericValue(str.charAt(i));
+                        i++;
+                    } 
+                }
+                return final_sum;
+            }
+            else{
+                return sum;
+            }
+            
         }
-
-        return sum;
+        return n;
     }
 }
