3f60ba35230a0defa5379fd9ec269a6c1df6aaf9
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2023-08-27 18:23:53 +0530

    Time: 0 ms (100.00%), Space: 39.1 MB (80.85%) - LeetHub

---
+++ 2544-alternating-digit-sum/2544-alternating-digit-sum.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int alternateDigitSum(int n) {
+        if (n <= 9) {
+            return n;
+        }
+
+        String str = Integer.toString(n);
+        int sum = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (i % 2 == 0) {
+                sum += Character.getNumericValue(str.charAt(i));  // Add even-positioned digits
+            } else {
+                sum -= Character.getNumericValue(str.charAt(i));  // Subtract odd-positioned digits
+            }
+        }
+
+        return sum;
+    }
+}
