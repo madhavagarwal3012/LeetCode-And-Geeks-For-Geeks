6217cccfb93a3eeeb6807a735f1e84a7c3c6075b
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2023-09-02 16:48:24 +0530

    Time: 3 ms (86.96%), Space: 45 MB (48.43%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
@@ -5,33 +5,38 @@ class Solution {
         int largest = Integer.MIN_VALUE;
         int second_largest = Integer.MIN_VALUE;
         int third_largest = Integer.MIN_VALUE;
-        int smallest = Integer.MAX_VALUE;
-        int second_smallest = Integer.MAX_VALUE;
-
+        
         while (start < end) {
-            int current = nums[start];
-
-            if (current > largest) {
+            if (nums[start] > largest) {
                 third_largest = second_largest;
                 second_largest = largest;
-                largest = current;
-            } else if (current > second_largest) {
+                largest = nums[start];
+            } else if (nums[start] > second_largest) {
                 third_largest = second_largest;
-                second_largest = current;
-            } else if (current > third_largest) {
-                third_largest = current;
+                second_largest = nums[start];
+            } else if (nums[start] > third_largest) {
+                third_largest = nums[start];
             }
-
-            if (current < smallest) {
-                second_smallest = smallest;
-                smallest = current;
-            } else if (current < second_smallest) {
-                second_smallest = current;
-            }
-
             start++;
         }
-
-        return Math.max(largest * second_largest * third_largest, largest * smallest * second_smallest);
+        
+        start = 0;
+        int smallest = Integer.MAX_VALUE;
+        int second_smallest = Integer.MAX_VALUE;
+        
+        while (start < end) {
+            if (nums[start] < smallest) {
+                second_smallest = smallest;
+                smallest = nums[start];
+            } else if (nums[start] < second_smallest) {
+                second_smallest = nums[start];
+            }
+            start++;
+        }
+        
+        int product1 = largest * second_largest * third_largest;
+        int product2 = largest * smallest * second_smallest;
+        
+        return Math.max(product1, product2);
     }
 }
