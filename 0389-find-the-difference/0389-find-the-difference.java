5bcae604b8bb5741b8d0e7e8cc78717a173bdcdf
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2023-09-08 23:10:00 +0530

    Time: 3 ms (45.73%), Space: 40.8 MB (48.49%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -2,29 +2,27 @@ class Solution {
     public static char findTheDifference(String s, String t) {
         char array[] = s.toCharArray();
         Arrays.sort(array);
-        s = new String (array);
-         
-        array= t.toCharArray();
+        s = new String(array);
+
+        array = t.toCharArray();
         Arrays.sort(array);
-        t = new String (array);
-      
-        int start=0;
-        int end=s.length();
-        int small_len=t.length();
-        char last_char=' ';
-        
-        if(t.length()>s.length()){
-            end=t.length();
-            small_len=s.length();
-            last_char=t.charAt(t.length()-1);
+        t = new String(array);
+
+        int start = 0;
+        int end = s.length();
+        int small_len = t.length();
+        char last_char = ' ';
+
+        if (t.length() > s.length()) {
+            end = t.length();
+            small_len = s.length();
+            last_char = t.charAt(t.length() - 1);
+        } else {
+            last_char = s.charAt(s.length() - 1);
         }
-      else{
-         last_char=s.charAt(s.length()-1);
-      }
-        
-        while(start<end){
-             //System.out.println(s.charAt(start)+" "+t.charAt(start));
-            if(start<small_len && s.charAt(start)!=t.charAt(start)){
+
+        while (start < end) {
+            if (start < small_len && s.charAt(start) != t.charAt(start)) {
                 return t.charAt(start);
             }
             start++;
