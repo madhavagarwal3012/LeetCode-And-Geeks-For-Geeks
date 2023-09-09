98dfb443bccd2407338f7612b784165d8a489bc7
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2023-09-09 22:23:55 +0530

    Time: 146 ms (26.79%), Space: 58.2 MB (9.56%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,22 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
     public static int countPrimes(int n) {
         if (n <= 2) {
             return 0;
         }
+
         boolean[] isComposite = new boolean[n];
-        int count = 0;
-        for (int start = 2; start * start < n; start++) {
-            if (!isComposite[start]) {
-                for (int multiple = start * start; multiple < n; multiple += start) {
+        List<Integer> primes = new ArrayList<>();
+        
+        for (int num = 2; num < n; num++) {
+            if (!isComposite[num]) {
+                primes.add(num);
+                for (int multiple = num * 2; multiple < n; multiple += num) {
                     isComposite[multiple] = true;
                 }
             }
         }
-        for (int i = 2; i < n; i++) {
-            if (!isComposite[i]) {
-                count++;
-            }
-        }
-        return count;
+
+        return primes.size();
     }
 }
