e5b043f8a54e287abb9bb6a3642321b7d6dcaac2
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2023-09-09 22:04:15 +0530

    Time: 105 ms (59.75%), Space: 48.4 MB (15.08%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -10,8 +10,6 @@ class Solution {
         for (int start = 2; start < n; start++) {
             if (!isComposite[start]) {
                 count++;
-
-                // Mark all multiples of the prime as composite
                 for (int multiple = 2 * start; multiple < n; multiple += start) {
                     isComposite[multiple] = true;
                 }
