bd585b85272346fa9c8d5ccb77ad9991d113b75c
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2024-02-14 03:05:28 +0530

    Time: 4 ms (86.86%), Space: 57.8 MB (82.83%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+        ListNode reverse = null;
+        ListNode reverseStore = null;
+        ListNode tail = head;
+        int count = 0;
+        int equalElement = 0;
+        
+        while(tail!=null){
+            tail = tail.next;
+            count++;
+        }
+        while(equalElement<count/2){
+            reverse = new ListNode(head.val);
+            if(equalElement==0){
+                reverseStore=reverse;
+                reverse.next=null;
+            }
+            reverse.next=reverseStore;
+            reverseStore=reverse;
+            head=head.next;
+            equalElement++;
+        }
+        if (count%2!=0 && equalElement==count/2) {
+            head=head.next;
+        }
+        reverse=reverseStore;
+        equalElement=0;
+        while(head!=null){
+            if(reverse.val==head.val){
+                equalElement++;
+            }
+            reverse=reverse.next;
+            head=head.next;
+        }
+        if(equalElement==count/2){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
