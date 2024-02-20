4feb9b47a4c83183d3b29bffc4b3f503c6a7e701
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2024-02-21 04:24:26 +0530

    Time: 2 ms (56.84%), Space: 45.2 MB (49.87%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
@@ -1,17 +1,17 @@
 class Solution {
     public int repeatedNTimes(int[] nums) {
-        int Element=0,repeatedElement=0,index=0;
+        int repeatedElement=0,index=0;
         
         while(index<nums.length){
-            Element=nums[index];
+            repeatedElement=nums[index];
             int subIndex=0;
             while(subIndex<nums.length){
                 if(subIndex==index){
                     subIndex++;
                 }
                 else{
-                    if(Element==nums[subIndex]){
-                        repeatedElement=Element;
+                    if(repeatedElement==nums[subIndex]){
+                        return repeatedElement;
                     }
                     subIndex++;
                 }
