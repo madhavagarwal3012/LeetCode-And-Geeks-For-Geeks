c3649ccb2ff419c7f078e8515b536d811dea198f
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2024-03-10 15:48:34 +0530

    Time: 2 ms (7.69%), Space: 41.8 MB (87.49%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -0,0 +1,51 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int index=0;
+        int index2=0;
+        while(index<m){
+            if(n>0){
+                if(nums1[index]<=nums2[index2]){
+                    index++;
+                }
+                else{
+                    int temp=nums1[index];
+                    nums1[index]=nums2[index2];
+                    nums2[index2]=temp;
+
+                    index++;
+
+                    if(n>1){
+                        if(index2<n-1 && nums2[index2]>nums2[index2+1]){
+                            int index3=index2;
+                            while(index3<n-1 && nums2[index3]>nums2[index3+1]){
+                                temp=nums2[index3+1];
+                                nums2[index3+1]=nums2[index3];
+                                nums2[index3]=temp;
+                                index3++;
+                            }
+                        }
+                    }
+                }
+            }
+            else{
+                return;
+            }
+        }
+        if(n>1){
+            if(index2<n-1 && nums2[index2]>nums2[index2+1]){
+                while(index2<n-1 && nums2[index2]>nums2[index2+1]){
+                    int temp=nums2[index2+1];
+                    nums2[index2+1]=nums2[index2];
+                    nums2[index2]=temp;
+                    index2++;
+                }
+            }
+        }
+        index2=0;
+        while(index2<n){
+            nums1[index]=nums2[index2];
+            index++;
+            index2++;
+        }
+    }
+}
