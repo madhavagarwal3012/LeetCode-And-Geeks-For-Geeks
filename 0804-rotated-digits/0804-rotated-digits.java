4853c992b5cd3f7c41a3f1077cdb25bd43fb0070
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2024-06-07 22:43:47 +0530

    Time: 13 ms (19.64%), Space: 43.6 MB (25%) - LeetHub

---
+++ 0804-rotated-digits/0804-rotated-digits.java
@@ -0,0 +1,35 @@
+class Solution {
+    public static int rotatedDigits(int n) {
+        int start = 2;
+        int count = 0;
+        boolean isRotated = false;
+        String str = "";
+
+        while (start <= n) {
+            isRotated = false;
+            str = Integer.toString(start);
+            int sub_start = 0;
+
+            while (sub_start < str.length()) {
+                if (str.charAt(sub_start) == '2' || str.charAt(sub_start) == '5' || 
+                    str.charAt(sub_start) == '6' || str.charAt(sub_start) == '9') {
+                    isRotated = true;
+                } 
+                else if (str.charAt(sub_start) == '3' || str.charAt(sub_start) == '4' || 
+                         str.charAt(sub_start) == '7') {
+                    isRotated = false;
+                    break;
+                }
+                sub_start++;
+            }
+
+            if (isRotated) {
+                count++;
+            }
+
+            start++;
+        }
+
+        return count;
+    }
+}
