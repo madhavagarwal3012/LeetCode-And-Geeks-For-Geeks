658318fa6f34fcdccc602b9c0dbdbd40b97eabae
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2024-06-08 12:01:28 +0530

    Time: 854 ms (5.09%), Space: 45.7 MB (6.66%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,16 +3,11 @@ class Solution {
         if(s.length() != t.length()) {
             return false; // If lengths are different, they cannot be anagrams
         }
-        
-        int sumS = 0;
-        int sumT = 0;
         String characters = "";
 
         for (int index = 0; index < s.length(); index++) {
-            sumS += (int) s.charAt(index); 
             for (int subIndex = 0; subIndex < t.length(); subIndex++) {
                 if(s.charAt(index) == t.charAt(subIndex)){
-                    sumT += (int) t.charAt(subIndex);
                     characters+=s.charAt(index);
                     t = t.replaceFirst(Character.toString(t.charAt(subIndex)), "");
                     break;
@@ -20,6 +15,6 @@ class Solution {
             }
             
         }
-        return sumS == sumT && characters.length() == s.length();
+        return characters.length() == s.length();
     }
 }
