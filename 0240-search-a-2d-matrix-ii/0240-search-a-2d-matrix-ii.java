9fa6de4db5db56bb302964690f3b65370322cab6
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2024-06-09 02:14:25 +0530

    Time: 35 ms (5.85%), Space: 45.8 MB (65.99%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        boolean present = false;
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+
+        for(int row=0; row < rows; row++){
+            for(int column=0; column < columns; column++){
+                if(matrix[row][column] == target){
+                    present = true;
+                }
+            }
+        }
+        return present;
+    }
+}
