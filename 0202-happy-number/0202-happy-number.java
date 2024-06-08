2fdf656511219cc6f09740aea7f406e39ecf98c7
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2024-06-08 09:49:59 +0530

    Time: 2 ms (20.87%), Space: 41.1 MB (13.55%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean isHappy(int n) {
+        if(n == 1){
+            return true;
+        }
+        else{
+            int sum = 0;
+            int check = 0;
+            int step = 0;
+            String num = String.valueOf(n);
+            while(sum!=1){
+                if(step>50){
+                    return false;
+                }
+                sum = 0;
+                int index = 0;
+                while(index<num.length()){
+                    int value = Character.getNumericValue(num.charAt(index));
+                    sum = sum + (value * value);
+                    if(step>10 && check == sum){
+                        return false;
+                    }
+                    index++;
+                }
+                if(sum == n){
+                    return false;
+                }
+                else{
+                    num = String.valueOf(sum);
+                }
+                if(check == 0){
+                    check = sum;
+                }
+                step++;
+            }
+        }
+        return true;
+    }
+}
