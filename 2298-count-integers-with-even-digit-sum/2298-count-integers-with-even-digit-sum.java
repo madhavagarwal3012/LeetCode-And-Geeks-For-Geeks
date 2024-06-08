2dd25432b1d321d8e9701becdb267ceea32ac0d9
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2024-06-09 04:19:59 +0530

    Time: 7 ms (5.9%), Space: 41.9 MB (5.45%) - LeetHub

---
+++ 2298-count-integers-with-even-digit-sum/2298-count-integers-with-even-digit-sum.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int countEven(int num) {
+        int digitSum = 0;
+        int countNum = 1;
+
+        while(countNum <= num){
+            int sum = 0;
+            int subIndex = 0;
+            String number = String.valueOf(countNum);
+            while(subIndex < number.length()){
+                sum = sum + Character.getNumericValue(number.charAt(subIndex));
+                subIndex++;
+            }
+            if(sum % 2 == 0){
+                digitSum++;
+            }
+            countNum++;
+        }
+        return digitSum++;
+    }
+}
