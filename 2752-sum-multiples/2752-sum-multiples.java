089b6c8112766d15af83c60f04180b55d0e06d01
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2024-06-10 07:05:00 +0530

    Time: 3 ms (56.97%), Space: 40.6 MB (69.72%) - LeetHub

---
+++ 2752-sum-multiples/2752-sum-multiples.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int sumOfMultiples(int n) {
+        int number = 1;
+        int sum = 0;
+        while(number <= n){
+            if(number % 3 == 0 || number % 5 == 0 || number % 7 == 0){
+                sum = sum + number;
+            }
+            number++;
+        }
+        return sum;
+    }
+}
