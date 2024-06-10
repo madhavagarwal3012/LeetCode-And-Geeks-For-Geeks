9863bde3e09216a70dad13f89bbe7e5644ed99a6
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2024-06-10 09:21:31 +0530

    Time: 1 ms (71.14%), Space: 40.3 MB (68.7%) - LeetHub

--- 2571-find-the-pivot-integer/2571-find-the-pivot-integer.java
+++ 2571-find-the-pivot-integer/2571-find-the-pivot-integer.java
@@ -1,18 +1,18 @@
 class Solution {
-    public int pivotInteger(int n) {
-        if(n <= 1){
-            return n;
+    public int pivotInteger(int Totalnumbers) {
+        if(Totalnumbers <= 1){
+            return Totalnumbers;
         }
         else{
-            int x = 1;
-            while(x <= n){
-                int sumtillPivot = x * (x +1) / 2;
-                int sumtillN = (x + n) * (n - x + 1) /2;
+            int number = 1;
+            while(number <= Totalnumbers){
+                int sumtillPivot = (number * (number +1))/ 2;       //Formula from arithmetic series
+                int sumtillN = (number + Totalnumbers) * (Totalnumbers - number + 1) /2;
 
                 if(sumtillPivot == sumtillN){
-                    return x;
+                    return number;
                 }
-                x++;
+                number++;
             }
         }
         return -1;
