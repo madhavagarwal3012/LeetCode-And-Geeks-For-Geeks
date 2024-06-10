aeab28c707eb93308a717c79ce2034d1fba2de4e
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2024-06-10 10:10:03 +0530

    Time: 9 ms (56.69%), Space: 41.6 MB (23.22%) - LeetHub

--- 2824-check-if-the-number-is-fascinating/2824-check-if-the-number-is-fascinating.java
+++ 2824-check-if-the-number-is-fascinating/2824-check-if-the-number-is-fascinating.java
@@ -7,10 +7,10 @@ class Solution {
 
         int number = 1;
         int count = 0;
-        int duplicate = 0;
+
         while(number <= 9){
             String digit = String.valueOf(number);
-            duplicate = 0;
+            int duplicate = 0;
             int index = 0;
             while(index < fascinating.length()){
                 if(number == Character.getNumericValue(fascinating.charAt(index))){
