681628348e8066b3f6ab6c0e693129237e6304f3
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2024-07-03 01:08:47 +0530

    Time: 1 ms (48.14%), Space: 57.2 MB (66.86%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,30 +1,14 @@
 class Solution {
     public void rotate(int[] nums, int k) {
         int n = nums.length;
-        if (n <= 1) {
-            return; // No need to rotate for arrays of size 0 or 1.
-        }
-
-        k = k % n; // Ensure k is within the range of the array length.
+        k = k % n; 
+        int[] result = new int[n];
         
-        int pointer = 0;
-        int end = n - 1;
-        int[] rotatedArr = new int[n];
-
-        while (pointer < k) {
-            rotatedArr[k - pointer - 1] = nums[end];
-            pointer++;
-            end--;
-        }
-
-        pointer = 0;
-        while (pointer < n - k) {
-            rotatedArr[k + pointer] = nums[pointer];
-            pointer++;
-        }
-
         for (int i = 0; i < n; i++) {
-            nums[i] = rotatedArr[i];
+            int pointer = (i + k) % n;
+            result[pointer] = nums[i];
         }
+        
+        System.arraycopy(result, 0, nums, 0, n);
     }
 }
