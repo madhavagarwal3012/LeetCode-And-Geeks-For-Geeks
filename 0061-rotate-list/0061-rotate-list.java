691f5a2a81c5d88c9f2106a6151aaea552611f35
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2024-07-03 01:07:32 +0530

    Time: 1 ms (11.8%), Space: 42.3 MB (50.33%) - LeetHub

---
+++ 0061-rotate-list/0061-rotate-list.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public static int [] rightRotation(ArrayList<Integer> nums, int R){
+		int size = nums.size();
+        R = R % size; 
+        int[] rotatedArray = new int[size];
+        
+        for (int index = 0; index < size; index++) {
+            int rightPointer = (index + R) % size;
+            rotatedArray[rightPointer] = nums.get(index);
+        }
+		return rotatedArray;
+	}
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head == null || k == 0){
+            return head;
+        }
+        ArrayList<Integer>list =  new ArrayList<>();
+        ListNode tail = head;
+        while(tail != null){
+            list.add(tail.val);
+            tail = tail.next;
+        }
+
+        int rotatedArray[] = new int[list.size()];
+        rotatedArray = rightRotation(list, k);
+        tail = head;
+        for(int index = 0; index < rotatedArray.length; index++){
+            tail.val = rotatedArray[index];
+            tail = tail.next;
+        }
+        return head;
+    }
+}
