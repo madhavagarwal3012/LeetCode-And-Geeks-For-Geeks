aaf4920fce469f5288213cfca4581a0ea23cc726
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2024-07-03 20:34:24 +0530

    Time: 2 ms (28.32%), Space: 57.1 MB (66.86%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,21 +1,14 @@
 class Solution {
-    public int[] arrayRotation(int [] array, int direction, char side){
-        direction = direction % array.length;
-        int [] rotatedArray = new int[array.length];
-
-        for(int index = 0; index < array.length; index++){
-            int newIndex = (index + direction) % array.length;
-            if(side == 'L'){
-                rotatedArray[index] = array[newIndex];
-            }
-            else if(side == 'R'){
-                rotatedArray[newIndex] = array[index];
-            }
-        }
-        return rotatedArray;
-    }
     public void rotate(int[] nums, int k) {
-        int[] result = arrayRotation(nums, k, 'R');
-        System.arraycopy(result, 0, nums, 0, nums.length);
+        int n = nums.length;
+        k = k % n; 
+        int[] result = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            int pointer = (i + k) % n;
+            result[pointer] = nums[i];
+        }
+        
+        System.arraycopy(result, 0, nums, 0, n);
     }
 }
