2d552ea388dab760bb914dd88ad0f33d6d1655ca
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2024-07-05 17:58:23 +0530

    Time: 6 ms (40.35%), Space: 53.1 MB (19.48%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,6 +1,24 @@
 class Solution {
     public int majorityElement(int[] nums) {
         Arrays.sort(nums);
-        return nums[nums.length/2];
+        int majorityCount = 0;
+        int majorityIndex = 0;
+        int count = 0;
+        for(int index = 0; index < nums.length; index++){
+            int subIndex = index + 1;
+            if(subIndex < nums.length){
+                if(nums[index] == nums[subIndex]){
+                    count++;
+                }
+                else{
+                    count = 0;
+                }
+            }
+            if(count > majorityCount){
+                majorityCount = count;
+                majorityIndex = index;
+            }
+        }
+        return nums[majorityIndex];
     }
 }
