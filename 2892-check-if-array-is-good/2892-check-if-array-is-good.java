a706325dc567098a8b6eb3bce52872525cebf2e8
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2024-07-13 17:05:17 +0530

    Time: 3 ms (45.44%), Space: 43.5 MB (8.01%) - LeetHub

---
+++ 2892-check-if-array-is-good/2892-check-if-array-is-good.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean isGood(int[] nums) {
+        int permutationCheck = 2;
+        int maximumNumber = Integer.MIN_VALUE;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int index = 0; index < nums.length; index++){
+            if(!map.containsKey(nums[index])){
+                map.put(nums[index],1);
+                if(maximumNumber < nums[index]){
+                    maximumNumber = nums[index];
+                }
+            }
+            else{
+                map.put(nums[index],map.get(nums[index])+1);
+            }
+        }
+        if(map.get(maximumNumber) == 2){
+            permutationCheck--;
+        }
+        else{
+            return false;
+        }
+        for(int index = 0; index < nums.length; index++){
+            if(nums[index] != maximumNumber){
+                if(map.get(nums[index]) > 1){
+                    permutationCheck--;
+                }
+            }
+        }
+        if(permutationCheck != 1){
+            return false;
+        }
+        else if(maximumNumber == nums.length-1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
