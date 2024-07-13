eb6dd86809daf45229483c979ab3adced8078759
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2024-07-13 15:29:18 +0530

    Time: 2 ms (96.61%), Space: 41.8 MB (44.93%) - LeetHub

---
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        ArrayList<Integer> list = new ArrayList<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        HashMap<Integer, Boolean> occurence = new HashMap<>();
+        for(int index = 0; index < arr.length; index++){
+            if(!map.containsKey(arr[index])){
+                map.put(arr[index],1);
+            }
+            else{
+                map.put(arr[index],map.get(arr[index])+1);
+            }
+        }
+        for(int frequency : map.values()){
+            if(!occurence.containsKey(frequency)){
+                occurence.put(frequency,true);
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
