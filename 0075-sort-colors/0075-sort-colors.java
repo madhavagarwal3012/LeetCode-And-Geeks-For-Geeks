0dfc9f353c7fbc92227dde5b238d52bf3e3d6cd9
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2024-07-16 00:51:35 +0530

    Time: 0 ms (100%), Space: 42 MB (13.56%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,27 +1,20 @@
 class Solution {
     public void sortColors(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int index = 0; index < nums.length; index++){
-            if(!map.containsKey(nums[index])){
-                map.put(nums[index], 1);
-            }
-            else{
-                map.put(nums[index], map.get(nums[index]) + 1);
-            }
-        }
         int redFrequency = 0;
         int whiteFrequency = 0;
         int blueFrequency = 0;
+        for(int index = 0; index < nums.length; index++){
+             if(nums[index] == 0){
+                redFrequency++;
+            }
+            else if(nums[index] == 1){
+                whiteFrequency++;
+            }
+            else if(nums[index] == 2){
+                blueFrequency++;
+            }
+        }
         for(int index = 0; index < nums.length;){
-            if(map.containsKey(0)){
-                redFrequency = map.get(0);
-            }
-            if(map.containsKey(1)){
-                whiteFrequency = map.get(1);
-            }
-            if(map.containsKey(2)){
-                blueFrequency = map.get(2);
-            }
             while(redFrequency > 0){
                 nums[index] = 0;
                 redFrequency--;
