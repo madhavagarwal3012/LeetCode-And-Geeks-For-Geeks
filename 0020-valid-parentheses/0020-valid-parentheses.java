1b581b11a351c4a79d7489441c0c98e669563e1f
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2024-07-16 18:45:20 +0530

    Time: 3 ms (13.25%), Space: 41.3 MB (72.94%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,48 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> openBracket = new Stack<>();
+        Stack<Character> closedBracket = new Stack<>();
+        for(int index = 0; index < s.length(); index++){
+            if(s.charAt(index) == '('){
+                openBracket.push(s.charAt(index));
+            }
+            else if(s.charAt(index) == '['){
+                openBracket.push(s.charAt(index));
+            }
+            else if(s.charAt(index) == '{'){
+                openBracket.push(s.charAt(index));
+            }
+            else if(s.charAt(index) == ')'){
+                closedBracket.push(s.charAt(index));
+                if(!openBracket.isEmpty()){
+                    if(openBracket.peek() == '('){
+                        openBracket.pop();
+                        closedBracket.pop();
+                    }
+                }
+            }
+            else if(s.charAt(index) == ']'){
+                closedBracket.push(s.charAt(index));
+                if(!openBracket.isEmpty()){
+                    if(openBracket.peek() == '['){
+                        openBracket.pop();
+                        closedBracket.pop();
+                    }
+                }
+            }
+            else if(s.charAt(index) == '}'){
+                closedBracket.push(s.charAt(index));
+                if(!openBracket.isEmpty()){
+                    if(openBracket.peek() == '{'){
+                        openBracket.pop();
+                        closedBracket.pop();
+                    }
+                }
+            }
+        }
+        if(openBracket.isEmpty() && closedBracket.isEmpty()){
+            return true;
+        }
+        return false;
+    }
+}
