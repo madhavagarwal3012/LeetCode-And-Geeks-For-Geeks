19074107dc00bc7a6980739ed3272b2662b12467
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2024-07-19 21:39:58 +0530

    Time: 2 ms (97.23%), Space: 44.7 MB (67.81%) - LeetHub

--- 3226-minimum-number-game/3226-minimum-number-game.java
+++ 3226-minimum-number-game/3226-minimum-number-game.java
@@ -2,9 +2,9 @@ class Solution {
     public int[] numberGame(int[] nums) {
         Arrays.sort(nums);
         for(int index = 0; index < nums.length - 1; index+=2){
-            int temp = nums[index + 1];
-            nums[index + 1] = nums[index];
-            nums[index] = temp;
+            int temp = nums[index];
+            nums[index] = nums[index + 1];
+            nums[index + 1] = temp;
         }
         return nums;
     }
