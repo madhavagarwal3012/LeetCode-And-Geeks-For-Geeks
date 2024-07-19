db4b9a69995ee712ba8787625f819a0011ef8180
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2024-07-19 06:45:22 +0530

    Time: 1 ms (76.58%), Space: 42.6 MB (54.89%) - LeetHub

--- 1574-maximum-product-of-two-elements-in-an-array/1574-maximum-product-of-two-elements-in-an-array.java
+++ 1574-maximum-product-of-two-elements-in-an-array/1574-maximum-product-of-two-elements-in-an-array.java
@@ -3,31 +3,29 @@ class Solution {
         if (nums.length < 2) {
             return 0; // Not enough elements to form a product
         }
-        HashMap<Integer, Integer> map = new HashMap<>();
+        int maximumFrequency = 1;
         int maximum = Integer.MIN_VALUE;
-        int secondMaximum = Integer.MIN_VALUE - 1;
+        int secondMaximum = Integer.MIN_VALUE;
 
         for(int index = 0; index < nums.length; index++){
-            if(!map.containsKey(nums[index])){
-                if(nums[index] > secondMaximum && nums[index] < maximum){
-                    secondMaximum = nums[index];
-                }
-                else if(nums[index] > maximum){
-                    secondMaximum = maximum;
-                    maximum = nums[index];
-                }
-                map.put(nums[index], 1);
+            if(maximum == nums[index]){
+                maximumFrequency++;
             }
-            else{
-                map.put(nums[index], map.get(nums[index]) + 1);
+            else if(nums[index] > secondMaximum && nums[index] < maximum){
+                secondMaximum = nums[index];
+            }
+            else if(nums[index] > maximum){
+                secondMaximum = maximum;
+                maximum = nums[index];
+                maximumFrequency = 1;
             }
         }
         int product = 1;
-        if(map.containsKey(maximum)){
-            if(map.get(maximum) > 1){
+        if(maximum != Integer.MIN_VALUE){
+            if(maximumFrequency > 1){
                 product = product * (maximum - 1) * (maximum - 1);
             }
-            else if(map.containsKey(secondMaximum)){
+            else if(secondMaximum != Integer.MIN_VALUE){
                 product = product * (secondMaximum - 1) * (maximum - 1);
             }
             else{
