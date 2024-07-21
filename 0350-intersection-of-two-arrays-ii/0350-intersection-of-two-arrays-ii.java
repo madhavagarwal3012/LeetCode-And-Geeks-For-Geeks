8ba0ab8285bef0a8e34d543fa0274f6a041dfa97
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2024-07-21 19:53:08 +0530

    Time: 3 ms (80.54%), Space: 43.1 MB (40.27%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -2,47 +2,27 @@ class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
         HashMap<Integer, Integer> map = new HashMap<>();
         ArrayList<Integer> intersectionList = new ArrayList<>();
-        if(nums1.length < nums2.length){
-            for(int index = 0; index < nums1.length; index++){
-                if(!map.containsKey(nums1[index])){
-                    map.put(nums1[index], 1);
-                }
-                else{
-                    map.put(nums1[index], map.get(nums1[index]) + 1);
-                }
-            }
-            for(int index = 0; index < nums2.length; index++){
-                if(map.containsKey(nums2[index]) && map.get(nums2[index]) > 0){
-                    intersectionList.add(nums2[index]);
-                    map.put(nums2[index], map.get(nums2[index]) - 1);
-                }
-            }
-            int intersectionArray[] = new int[intersectionList.size()];
-            for(int index = 0; index < intersectionArray.length; index++){
-                intersectionArray[index] = intersectionList.get(index);
-            }
-            return intersectionArray;
+        if(nums2.length < nums1.length){
+            return intersect(nums2, nums1);
         }
-        else{
-            for(int index = 0; index < nums2.length; index++){
-                if(!map.containsKey(nums2[index])){
-                    map.put(nums2[index], 1);
-                }
-                else{
-                    map.put(nums2[index], map.get(nums2[index]) + 1);
-                }
+        for(int index = 0; index < nums1.length; index++){
+            if(!map.containsKey(nums1[index])){
+                map.put(nums1[index], 1);
             }
-            for(int index = 0; index < nums1.length; index++){
-                if(map.containsKey(nums1[index]) && map.get(nums1[index]) > 0){
-                    intersectionList.add(nums1[index]);
-                    map.put(nums1[index], map.get(nums1[index]) - 1);
-                }
+            else{
+                map.put(nums1[index], map.get(nums1[index]) + 1);
             }
-            int intersectionArray[] = new int[intersectionList.size()];
-            for(int index = 0; index < intersectionArray.length; index++){
-                intersectionArray[index] = intersectionList.get(index);
-            }
-            return intersectionArray;
         }
+        for(int index = 0; index < nums2.length; index++){
+            if(map.containsKey(nums2[index]) && map.get(nums2[index]) > 0){
+                intersectionList.add(nums2[index]);
+                map.put(nums2[index], map.get(nums2[index]) - 1);
+            }
+        }
+        int intersectionArray[] = new int[intersectionList.size()];
+        for(int index = 0; index < intersectionArray.length; index++){
+            intersectionArray[index] = intersectionList.get(index);
+        }
+        return intersectionArray;
     }
 }
