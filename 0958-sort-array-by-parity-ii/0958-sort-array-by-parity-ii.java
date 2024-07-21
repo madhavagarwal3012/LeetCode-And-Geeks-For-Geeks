c7bd27ac5bc7db474104a81fc416054082aefd31
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2024-07-21 22:18:07 +0530

    Time: 6 ms (10.9%), Space: 47.1 MB (48.1%) - LeetHub

--- 0958-sort-array-by-parity-ii/0958-sort-array-by-parity-ii.java
+++ 0958-sort-array-by-parity-ii/0958-sort-array-by-parity-ii.java
@@ -2,42 +2,26 @@ class Solution {
     public int[] sortArrayByParityII(int[] nums) {
         ArrayList<Integer> evenList = new ArrayList<>();
         ArrayList<Integer> oddList = new ArrayList<>();
-        
-        ArrayList<Integer> evenIndex = new ArrayList<>();
-        ArrayList<Integer> oddIndex = new ArrayList<>();
+
+        int evenIndex = 0;
+        int oddIndex = 0;
 
         for(int index = 0; index < nums.length; index++){
-            if(index % 2 == 0){
-                if(nums[index] % 2 == 1){
-                    oddList.add(nums[index]);
-                    oddIndex.add(index);
-                }
-                if(!evenList.isEmpty() && !oddList.isEmpty()){
-                    nums[evenIndex.get(0)] = oddList.get(0);
-                    nums[oddIndex.get(0)] = evenList.get(0);
-
-                    oddList.remove(0);
-                    oddIndex.remove(0);
-
-                    evenList.remove(0);
-                    evenIndex.remove(0);
-                }
+            if(nums[index] % 2 == 0){
+                evenList.add(nums[index]);
             }
             else{
-                if(nums[index] % 2 == 0){
-                    evenList.add(nums[index]);
-                    evenIndex.add(index);
-                }
-                if(!evenList.isEmpty() && !oddList.isEmpty()){
-                    nums[evenIndex.get(0)] = oddList.get(0);
-                    nums[oddIndex.get(0)] = evenList.get(0);
-
-                    oddList.remove(0);
-                    oddIndex.remove(0);
-
-                    evenList.remove(0);
-                    evenIndex.remove(0);
-                }
+                oddList.add(nums[index]);
+            }
+        }
+        for(int index = 0; index < nums.length; index++){
+            if(index % 2 == 0){
+                nums[index] = evenList.get(evenIndex);
+                evenIndex++;
+            }
+            else{
+                nums[index] = oddList.get(oddIndex);
+                oddIndex++;
             }
         }
         return nums;
