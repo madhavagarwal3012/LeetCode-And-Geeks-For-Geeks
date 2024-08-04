225e5aaf55bd7d37e5c34c1de11b8e31a8dc4a83
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2024-08-04 22:03:37 +0530

    Time: 2 ms (98.22%), Space: 43.6 MB (63.76%) - LeetHub

--- 3471-minimum-average-of-smallest-and-largest-elements/3471-minimum-average-of-smallest-and-largest-elements.java
+++ 3471-minimum-average-of-smallest-and-largest-elements/3471-minimum-average-of-smallest-and-largest-elements.java
@@ -1,35 +1,51 @@
+// My First Approach
+// class Solution {
+//     public double minimumAverage(int[] nums) {
+//         int minimumIndex = 0;
+//         int maximumIndex = 0;
+//         double average = Double.MAX_VALUE;
+
+//         int minimumValue = Integer.MAX_VALUE;
+//         int maximumValue = Integer.MIN_VALUE;
+
+//         for(int steps = 0; steps < nums.length/2; steps++){
+//             for(int index = 0; index < nums.length; index++){
+//                 if(nums[index] > maximumValue && nums[index] != Integer.MAX_VALUE){
+//                     maximumValue = nums[index];
+//                     maximumIndex = index;
+//                 }
+//                 if(nums[index] < minimumValue && nums[index] != Integer.MIN_VALUE){
+//                     minimumValue = nums[index];
+//                     minimumIndex = index;
+//                 }
+//             }
+//             nums[maximumIndex] = Integer.MAX_VALUE;
+//             nums[minimumIndex] = Integer.MIN_VALUE;
+
+//             maximumIndex = 0;
+//             minimumIndex = 0;
+            
+//             if((maximumValue + minimumValue) * 1.0 / 2 < average){
+//                 average = (maximumValue + minimumValue) * 1.0 / 2;
+//             }
+            
+//             maximumValue = Integer.MIN_VALUE;
+//             minimumValue = Integer.MAX_VALUE;
+//         }
+//         return average;
+//     }
+// }
+
+// My Second Approach
 class Solution {
     public double minimumAverage(int[] nums) {
-        int minimumIndex = 0;
-        int maximumIndex = 0;
         double average = Double.MAX_VALUE;
+        Arrays.sort(nums);
 
-        int minimumValue = Integer.MAX_VALUE;
-        int maximumValue = Integer.MIN_VALUE;
-
-        for(int steps = 0; steps < nums.length/2; steps++){
-            for(int index = 0; index < nums.length; index++){
-                if(nums[index] > maximumValue && nums[index] != Integer.MAX_VALUE){
-                    maximumValue = nums[index];
-                    maximumIndex = index;
-                }
-                if(nums[index] < minimumValue && nums[index] != Integer.MIN_VALUE){
-                    minimumValue = nums[index];
-                    minimumIndex = index;
-                }
+        for(int index = 0, length = nums.length - 1; index <= length; index++, length--){
+            if((nums[index] + nums[length]) * 1.0 / 2 < average){
+                average = (nums[index] + nums[length]) * 1.0 / 2;
             }
-            nums[maximumIndex] = Integer.MAX_VALUE;
-            nums[minimumIndex] = Integer.MIN_VALUE;
-
-            maximumIndex = 0;
-            minimumIndex = 0;
-            
-            if((maximumValue + minimumValue) * 1.0 / 2 < average){
-                average = (maximumValue + minimumValue) * 1.0 / 2;
-            }
-            
-            maximumValue = Integer.MIN_VALUE;
-            minimumValue = Integer.MAX_VALUE;
         }
         return average;
     }
