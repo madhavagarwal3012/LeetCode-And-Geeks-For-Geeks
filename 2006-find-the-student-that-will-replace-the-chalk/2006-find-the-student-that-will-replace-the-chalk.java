22dfc37f449aab2be7a1c80a5a6e009ced2850a3
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2024-09-02 16:22:01 +0530

    Time: 1776 ms (17.41%), Space: 59.6 MB (74.79%) - LeetHub

--- 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.java
+++ 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.java
@@ -1,7 +1,7 @@
 class Solution {
     public int chalkReplacer(int[] chalk, int k) {
         int index = 0;
-        while(k >= chalk[index] && k > 0){
+        while(k >= chalk[index]){
             k = k - chalk[index];
             index++;
             if(index == chalk.length){
