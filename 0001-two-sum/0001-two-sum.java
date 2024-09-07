6b7143992ce84af397da91288c386300ea337c41
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2024-09-08 01:24:53 +0530

    Time: 3 ms (67.41%), Space: 44.8 MB (48.47%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,3 +1,29 @@
+//Brute Force Approach
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         int start=0;
+//         int end=nums.length;
+
+//         int arr[]=new int[2];
+//         int k=0;
+//         while(start<end){
+//             int j=start+1;
+//             while(j<end){
+//                  if(nums[start]+nums[j]==target){
+//                     arr[k]=start;
+//                     arr[k+1]=j;
+//             }
+//             j++;
+               
+//         }
+//         start++;
+//         }
+//         return arr;
+//     }
+// }
+
+
+//HashMap Approach (Optimal Approach)
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int positions[] = new int[2];
