1900bd1f7d619fd2c827f88945ea49bd61edda7d
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2024-09-23 00:10:25 +0530

    Time: 2 ms (19%), Space: 45.3 MB (84.6%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -5,12 +5,12 @@ class Solution {
                 if(matrix[row][column] == 0){
                     for(int columnIndex = 0; columnIndex < matrix[0].length; columnIndex++){
                         if(matrix[row][columnIndex] != 0){
-                            matrix[row][columnIndex] = 786;
+                            matrix[row][columnIndex] = 3012;
                         }
                     }
                     for(int rowIndex = 0; rowIndex < matrix.length; rowIndex++){
                         if(matrix[rowIndex][column] != 0){
-                            matrix[rowIndex][column] = 786;
+                            matrix[rowIndex][column] = 3012;
                         }
                     }
                 }
@@ -18,7 +18,7 @@ class Solution {
         }
         for(int row = 0; row < matrix.length; row++){
             for(int column = 0; column < matrix[0].length; column++){
-                if(matrix[row][column] == 786){
+                if(matrix[row][column] == 3012){
                     matrix[row][column] = 0;
                 }
             }
