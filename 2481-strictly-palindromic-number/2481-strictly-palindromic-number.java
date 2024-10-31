3b1178757528e165ce9584f5f7aba7326546fd52
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2024-11-01 01:51:37 +0530

    Time: 8 ms (6%), Space: 40.8 MB (6.2%) - LeetHub

--- 2481-strictly-palindromic-number/2481-strictly-palindromic-number.java
+++ 2481-strictly-palindromic-number/2481-strictly-palindromic-number.java
@@ -1,5 +1,16 @@
 class Solution {
     public boolean isStrictlyPalindromic(int n) {
-        return false;
+        for(int baseCount = 2; baseCount <= n - 2; baseCount++){
+            String number = Integer.toString(n, baseCount);
+            String reversedNumber = "";
+
+            for(int index = number.length() - 1; index >= 0; index--){
+                reversedNumber += number.charAt(index);
+            }
+            if(!number.equals(reversedNumber)){
+                return false;
+            }
+        }
+        return true;
     }
 }
