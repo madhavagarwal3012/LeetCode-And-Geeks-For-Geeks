770fcef3db0c7ada8ccf2994a678c6c104da8739
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2024-11-02 03:20:56 +0530

    Time: 11 ms (21.95%), Space: 41.7 MB (12.74%) - LeetHub

--- 0900-reordered-power-of-2/0900-reordered-power-of-2.java
+++ 0900-reordered-power-of-2/0900-reordered-power-of-2.java
@@ -12,31 +12,31 @@ class Solution {
             }
             base *= 2;
         }
+        HashMap<Character, Integer> numberDigitMap = new HashMap<>();
+        for(int numberIndex = 0; numberIndex < number.length(); numberIndex++){
+            if(!numberDigitMap.containsKey(number.charAt(numberIndex))){
+                System.out.println("intruder" + " "+ number.charAt(numberIndex));
+                numberDigitMap.put(number.charAt(numberIndex), 1);
+            }
+            else{
+                numberDigitMap.put(number.charAt(numberIndex), numberDigitMap.get(number.charAt(numberIndex)) + 1);
+            }
+        }
         for(int index = 0; index < twoPowerList.size(); index++){
+            boolean check = true;
             HashMap<Character, Integer> digitMap = new HashMap<>();
             number = twoPowerList.get(index);
             for(int numberIndex = 0; numberIndex < number.length(); numberIndex++){
                 if(!digitMap.containsKey(number.charAt(numberIndex))){
+                    if(!numberDigitMap.containsKey(number.charAt(numberIndex))){
+                        check = false;
+                        break;
+                    }
                     digitMap.put(number.charAt(numberIndex), 1);
                 }
                 else{
                     digitMap.put(number.charAt(numberIndex), digitMap.get(number.charAt(numberIndex)) + 1);
-                }
-            }
-            boolean check = true;
-            HashMap<Character, Integer> numberDigitMap = new HashMap<>();
-            number = String.valueOf(n);
-            for(int numberIndex = 0; numberIndex < number.length(); numberIndex++){
-                if(!numberDigitMap.containsKey(number.charAt(numberIndex))){
-                    if(!digitMap.containsKey(number.charAt(numberIndex))){
-                        check = false;
-                        break;
-                    }
-                    numberDigitMap.put(number.charAt(numberIndex), 1);
-                }
-                else{
-                    numberDigitMap.put(number.charAt(numberIndex), numberDigitMap.get(number.charAt(numberIndex)) + 1);
-                    if(numberDigitMap.get(number.charAt(numberIndex)) > digitMap.get(number.charAt(numberIndex))){
+                    if(digitMap.get(number.charAt(numberIndex)) > numberDigitMap.get(number.charAt(numberIndex))){
                         check = false;
                         break;
                     }
