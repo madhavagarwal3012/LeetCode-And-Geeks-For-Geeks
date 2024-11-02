a6a823d67753363fa77b6e74f7a992abd286259d
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2024-11-02 22:06:48 +0530

    Time: 1 ms (96.81%), Space: 42 MB (37.29%) - LeetHub

--- 2580-circular-sentence/2580-circular-sentence.java
+++ 2580-circular-sentence/2580-circular-sentence.java
@@ -1,12 +1,10 @@
 class Solution {
     public boolean isCircularSentence(String sentence) {
-        boolean checkWords = false;
         for(int index = 0; index < sentence.length(); index++){
             if(sentence.charAt(index) == ' '){
                 if(sentence.charAt(index - 1) != sentence.charAt(index + 1)){
                     return false;
                 }
-                checkWords = true;
             }
         }
         return sentence.charAt(0) == sentence.charAt(sentence.length() - 1);
