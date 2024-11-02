3f367f16962c17256a87fbf6147c06d727adaa82
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2024-11-03 01:19:33 +0530

    Time: 1 ms (98.79%), Space: 43.3 MB (87.51%) - LeetHub

--- 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.java
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.java
@@ -1,19 +1,13 @@
 class Solution {
     public int[] finalPrices(int[] prices) {
-        int pricesAfterDiscount [] = new int[prices.length];
         for(int index = 0; index < prices.length; index++){
-            boolean checkDiscount = false;
             for(int subIndex = index + 1; subIndex < prices.length; subIndex++){
                 if(prices[subIndex] <= prices[index]){
-                    pricesAfterDiscount[index] = prices[index] - prices[subIndex];
-                    checkDiscount = true;
+                    prices[index] = prices[index] - prices[subIndex];
                     break;
                 }
             }
-            if(!checkDiscount){
-                pricesAfterDiscount[index] = prices[index];
-            }
         }
-        return pricesAfterDiscount;
+        return prices;
     }
 }
