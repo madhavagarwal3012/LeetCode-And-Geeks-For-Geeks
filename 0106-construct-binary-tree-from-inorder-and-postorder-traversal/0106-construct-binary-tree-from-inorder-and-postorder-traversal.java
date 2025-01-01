2b50167a63a3f9535f2af8e5f5c3c3c4f4e290a7
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2025-01-02 04:00:31 +0530

    Time: 3 ms (27.88%), Space: 44 MB (92.74%) - LeetHub

---
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode buildTreeHelper(int[] inorder, int[] postorder, int inStart, int inEnd, int postStart, int postEnd){
+        if(inStart > inEnd || postStart > postEnd){
+            return null;
+        }
+        int rootIndex = -1;
+        for(int index = inStart; index <= inEnd; index++){
+            if(postorder[postEnd] == inorder[index]){
+                rootIndex = index;
+                break;
+            }
+        }
+
+
+        int inStartLeft = inStart;
+        int inEndLeft = rootIndex - 1;
+        int inStartRight = rootIndex + 1;
+        int inEndRight = inEnd;
+
+        int postStartLeft = postStart;
+        int postEndLeft = postStartLeft + (inEndLeft - inStartLeft);
+        int postStartRight = postEndLeft + 1;
+        int postEndRight = postEnd - 1;
+
+        TreeNode root = new TreeNode(postorder[postEnd]);
+        root.left = buildTreeHelper(inorder, postorder, inStartLeft, inEndLeft, postStartLeft, postEndLeft);
+        root.right = buildTreeHelper(inorder, postorder, inStartRight, inEndRight, postStartRight, postEndRight);
+        return root;
+    }
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        return buildTreeHelper(inorder, postorder, 0, inorder.length - 1, 0, postorder.length - 1);
+    }
+}
