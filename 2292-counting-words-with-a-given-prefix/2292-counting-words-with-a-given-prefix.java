70f91eac684b21b5cec0efedf1be443df74bc305
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2025-01-09 06:24:54 +0530

    Time: 22 ms (6.21%), Space: 45.2 MB (5.1%) - LeetHub

--- 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.java
+++ 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.java
@@ -4,7 +4,7 @@ class Solution {
         for(int index = 0; index < words.length; index++){
             String word = words[index];
             String prefCheck = "";
-            for(int wordIndex = 0; wordIndex < word.length() && wordIndex < pref.length(); wordIndex++){
+            for(int wordIndex = 0; wordIndex < word.length() && wordIndex < pref.length() && word.length() >= pref.length(); wordIndex++){
                 if(word.charAt(wordIndex) == pref.charAt(wordIndex)){
                     prefCheck += word.charAt(wordIndex);
                 }
