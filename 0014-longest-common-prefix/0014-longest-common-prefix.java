3d0dabf8a7396e2867cbf29a73d2a2cfc791bb19
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2025-03-22 01:40:26 +0530

    Time: 1 ms (65.09%), Space: 41.5 MB (76.38%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,30 +1,31 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        if(strs.length == 1){
+        if (strs.length == 1) {
             return strs[0];
         }
-        String commonPrefix = "";
+
+        StringBuilder commonPrefix = new StringBuilder();
         String firstString = strs[0];
         String secondString = strs[1];
 
-        for(int index = 0; index < firstString.length() && index < secondString.length(); index++){
-            if(firstString.charAt(index) != secondString.charAt(index)){
+        for (int index = 0; index < firstString.length() && index < secondString.length(); index++) {
+            if (firstString.charAt(index) != secondString.charAt(index)) {
                 break;
             }
-            commonPrefix+=firstString.charAt(index);
+            commonPrefix.append(firstString.charAt(index));
         }
-        for(int strsIndex = 2; strsIndex < strs.length; strsIndex++){
-            String commonChars = "";
-            for(int index = 0; index < commonPrefix.length() && index < strs[strsIndex].length(); index++){
-                if(commonPrefix.charAt(index) != strs[strsIndex].charAt(index)){
+
+        for (int strsIndex = 2; strsIndex < strs.length; strsIndex++) {
+            int newLength = 0;
+            for (int index = 0; index < commonPrefix.length() && index < strs[strsIndex].length(); index++) {
+                if (commonPrefix.charAt(index) != strs[strsIndex].charAt(index)) {
                     break;
                 }
-                else{
-                    commonChars+=commonPrefix.charAt(index);
-                }
+                newLength++;
             }
-            commonPrefix = commonChars;
+            commonPrefix.setLength(newLength);
         }
-        return commonPrefix;
+
+        return commonPrefix.toString();
     }
 }
