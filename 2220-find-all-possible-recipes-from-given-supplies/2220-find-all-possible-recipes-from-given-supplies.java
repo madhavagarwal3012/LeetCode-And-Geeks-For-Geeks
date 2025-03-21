69deafbbd2b1b1cc3011cc942b952399f74d51aa
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2025-03-21 20:38:42 +0530

    Time: 40 ms (77.37%), Space: 46.7 MB (70.53%) - LeetHub

---
+++ 2220-find-all-possible-recipes-from-given-supplies/2220-find-all-possible-recipes-from-given-supplies.java
@@ -0,0 +1,58 @@
+class Solution {
+    public List<String> findAllRecipes(String[] recipes, List<List<String>> ingredients, String[] supplies) {
+        List<String> successfullRecipesList = new ArrayList<>();
+        List<String> remainingIngredientsList;
+        HashMap<String, Boolean> supplyMap = new HashMap<>();
+        HashMap<List<String>, String> remainingRecipesMap = new HashMap<>();
+
+        List<List<String>> unsuccessfullRecipes = new ArrayList<>();
+
+        for(int supplyIndex = 0; supplyIndex < supplies.length; supplyIndex++){
+            supplyMap.put(supplies[supplyIndex], true);
+        }
+
+        for(int recipeIndex = 0; recipeIndex < recipes.length; recipeIndex++){
+            int ingredientsCheckCount = 0;
+            remainingIngredientsList = new ArrayList<>();
+            for(int ingredientIndex = 0; ingredientIndex < ingredients.get(recipeIndex).size(); ingredientIndex++){
+                if(supplyMap.containsKey(ingredients.get(recipeIndex).get(ingredientIndex))){
+                    ingredientsCheckCount++;
+                }
+                else{
+                    remainingIngredientsList.add(ingredients.get(recipeIndex).get(ingredientIndex));
+                }
+            }
+            if(ingredientsCheckCount == ingredients.get(recipeIndex).size()){
+                supplyMap.put(recipes[recipeIndex],true);
+                successfullRecipesList.add(recipes[recipeIndex]);
+            }
+            else{
+                remainingRecipesMap.put(remainingIngredientsList, recipes[recipeIndex]);
+                unsuccessfullRecipes.add(remainingIngredientsList);
+            }
+        }
+        boolean newRecipeAdded = true;
+        while (newRecipeAdded) {
+            newRecipeAdded = false;
+            for (int i = 0; i < unsuccessfullRecipes.size(); i++) {
+                int ingredientsCheckCount = 0;
+
+                for (int remainingIngredientIndex = 0; remainingIngredientIndex < unsuccessfullRecipes.get(i).size(); remainingIngredientIndex++) {
+                    if (supplyMap.containsKey(unsuccessfullRecipes.get(i).get(remainingIngredientIndex))) {
+                        ingredientsCheckCount++;
+                    }
+                }
+
+                if (ingredientsCheckCount == unsuccessfullRecipes.get(i).size()) {
+                    String recipe = remainingRecipesMap.get(unsuccessfullRecipes.get(i));
+                    supplyMap.put(recipe, true);
+                    successfullRecipesList.add(recipe);
+                    unsuccessfullRecipes.remove(i);
+                    i--;
+                    newRecipeAdded = true;
+                }
+            }
+        }
+        return successfullRecipesList;
+    }
+}
