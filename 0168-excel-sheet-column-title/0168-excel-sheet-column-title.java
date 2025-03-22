2025dd88b1fd5c975f78bf622865060a47fe16f2
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2025-03-22 23:39:11 +0530

    Time: 1 ms (17.32%), Space: 41.3 MB (15.06%) - LeetHub

--- 0168-excel-sheet-column-title/0168-excel-sheet-column-title.java
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.java
@@ -1,27 +1,27 @@
 class Solution {
     public String convertToTitle(int columnNumber) {
         HashMap<Integer, String> map = new HashMap<>();
-        for (int order = 1; order <= 26; order++) {
-            map.put(order, "" + (char) ('A' + (order - 1)));
+        for(int order = 1; order <= 26; order++){
+            map.put(order, "" + (char)('A' + (order - 1)));
         }
         String excelColumn = "";
-        if (columnNumber > 26 && columnNumber < 100) {
-            if (columnNumber % 26 != 0) {
+        if(columnNumber > 26 && columnNumber < 100){
+            if(columnNumber % 26 != 0){
                 int quotient = columnNumber / 26;
                 int divisibleValue = 26 * quotient;
-                excelColumn = map.get(quotient) + map.get(columnNumber - divisibleValue);
-            } 
-            else {
+                excelColumn = map.get(divisibleValue/26) + map.get(columnNumber - divisibleValue);
+            }
+            else{
                 excelColumn = map.get(columnNumber / 26 - 1) + map.get(26);
             }
-        } 
-        else {
-            while (columnNumber > 26) {
-                if (columnNumber % 26 == 0) {
+        }
+        else{
+            while(columnNumber > 26){
+                if(columnNumber % 26 == 0){
                     excelColumn = map.get(26) + excelColumn;
-                    columnNumber = columnNumber / 26 - 1;  // Decrease by 1 to adjust for 1-based indexing
-                } 
-                else {
+                    columnNumber = columnNumber / 26 - 1;
+                }
+                else{
                     int quotient = columnNumber / 26;
                     int divisibleValue = 26 * quotient;
                     excelColumn = map.get(columnNumber - divisibleValue) + excelColumn;
@@ -30,7 +30,6 @@ class Solution {
             }
             excelColumn = map.get(columnNumber) + excelColumn;
         }
-        
         return excelColumn;
     }
 }
