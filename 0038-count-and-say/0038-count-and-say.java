5ff6dfe8e78bd04b67a3f6bb0220a219716047fc
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2025-03-22 17:09:15 +0530

    Time: 10 ms (26.53%), Space: 41.7 MB (41.45%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.java
+++ 0038-count-and-say/0038-count-and-say.java
@@ -8,21 +8,14 @@ class Solution {
             StringBuilder number = new StringBuilder();
             boolean lastValueFlag = true;
             int frequency = 1;
-            for(int index = 0; index < cs.length() - 1; index++){
-                if(cs.charAt(index) == cs.charAt(index + 1)){
+            for(int index = 0; index < cs.length(); index++){
+                if(index < cs.length() - 1 && cs.charAt(index) == cs.charAt(index + 1)){
                     frequency++;
-                    if(index + 1 == cs.length() - 1){
-                        lastValueFlag = false;
-                        number.append(frequency).append(cs.charAt(index));
-                    }
                 }
                 else{
                     number.append(frequency).append(cs.charAt(index));
                     frequency = 1;
                 }
-            }
-            if(lastValueFlag){
-                number.append(1).append(cs.charAt(cs.length() - 1));
             }
             cs = number.toString();
         }
