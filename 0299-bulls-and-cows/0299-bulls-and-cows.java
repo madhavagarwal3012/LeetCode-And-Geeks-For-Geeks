fb9e38cbce54b64a8452257626c9e43aeeb6670b
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2025-03-23 01:16:08 +0530

    Time: 10 ms (15.24%), Space: 43.8 MB (7.19%) - LeetHub

---
+++ 0299-bulls-and-cows/0299-bulls-and-cows.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String getHint(String secret, String guess) {
+        HashMap<Character, Integer> secretMap = new HashMap<>();
+        for(int index = 0; index < secret.length(); index++){
+            if(!secretMap.containsKey(secret.charAt(index))){
+                secretMap.put(secret.charAt(index), 1);
+            }
+            else{
+                secretMap.put(secret.charAt(index), secretMap.get(secret.charAt(index)) + 1);
+            }
+        }
+        int bulls = 0;
+        int cows = 0;
+        for(int index = 0; index < secret.length(); index++){
+            if(secret.charAt(index) == guess.charAt(index)){
+                if(secretMap.get(guess.charAt(index)) > 0){
+                    secretMap.put(guess.charAt(index), secretMap.get(guess.charAt(index)) - 1);
+                    bulls++;
+                }
+                else{
+                    secretMap.remove(guess.charAt(index));
+                }
+            }
+        }
+        for(int index = 0; index < secret.length(); index++){
+            if(secret.charAt(index) != guess.charAt(index) && secretMap.containsKey(guess.charAt(index))){
+                if(secretMap.get(guess.charAt(index)) > 0){
+                    secretMap.put(guess.charAt(index), secretMap.get(guess.charAt(index)) - 1);
+                    cows++;
+                }
+            }
+        }
+        return String.valueOf(bulls) + "A" + cows + "B";
+    }
+}
