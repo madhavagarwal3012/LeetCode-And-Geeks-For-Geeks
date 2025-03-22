f9e3ab8f3b4dd8e0b1ec1270b3e7c376d314fd95
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2025-03-23 03:21:55 +0530

    Time: 745 ms (14.18%), Space: 41.5 MB (73.57%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -2,13 +2,11 @@ class Solution {
     public int countSubstrings(String s) {
         int palindromicCount = 0;
         for(int index = 0; index < s.length(); index++){
-            StringBuilder substring = new StringBuilder();
             for(int substringIndex = index; substringIndex < s.length(); substringIndex++){
-                substring.append(s.charAt(substringIndex));
-                int startingIndex = 0;
-                int endingIndex = substring.length() - 1;
+                int startingIndex = index;
+                int endingIndex = substringIndex;
                 while(startingIndex < endingIndex){
-                    if(substring.charAt(startingIndex) == substring.charAt(endingIndex)){
+                    if(s.charAt(startingIndex) == s.charAt(endingIndex)){
                         startingIndex++;
                         endingIndex--;
                     }
