41c23ddb76b0e0b0d05eed701e18034219b5aa3f
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2025-03-22 23:27:31 +0530

    Time: 1 ms (17.32%), Space: 41.2 MB (29.78%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.java
@@ -0,0 +1,36 @@
+class Solution {
+    public String convertToTitle(int columnNumber) {
+        HashMap<Integer, String> map = new HashMap<>();
+        for (int order = 1; order <= 26; order++) {
+            map.put(order, "" + (char) ('A' + (order - 1)));
+        }
+        String excelColumn = "";
+        if (columnNumber > 26 && columnNumber < 100) {
+            if (columnNumber % 26 != 0) {
+                int quotient = columnNumber / 26;
+                int divisibleValue = 26 * quotient;
+                excelColumn = map.get(quotient) + map.get(columnNumber - divisibleValue);
+            } 
+            else {
+                excelColumn = map.get(columnNumber / 26 - 1) + map.get(26);
+            }
+        } 
+        else {
+            while (columnNumber > 26) {
+                if (columnNumber % 26 == 0) {
+                    excelColumn = map.get(26) + excelColumn;
+                    columnNumber = columnNumber / 26 - 1;  // Decrease by 1 to adjust for 1-based indexing
+                } 
+                else {
+                    int quotient = columnNumber / 26;
+                    int divisibleValue = 26 * quotient;
+                    excelColumn = map.get(columnNumber - divisibleValue) + excelColumn;
+                    columnNumber = quotient;
+                }
+            }
+            excelColumn = map.get(columnNumber) + excelColumn;
+        }
+        
+        return excelColumn;
+    }
+}
