7679219057a1786f23561da10d39d4cf92a7df1b
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2025-03-24 23:59:49 +0530

    Time: 331 ms (5.05%), Space: 100.5 MB (55.56%) - LeetHub

--- 3430-count-days-without-meetings/3430-count-days-without-meetings.java
+++ 3430-count-days-without-meetings/3430-count-days-without-meetings.java
@@ -3,30 +3,30 @@ class Solution {
         int countD = 0;
         Arrays.sort(meetings, (a, b) -> Integer.compare(a[0], b[0]));
         int day = 1;            int row = 0;
-            while(row < meetings.length){
-                if(day >= meetings[row][0]){
-                    if(day <= meetings[row][1] || meetings[row][1] > day){
-                      day = meetings[row][1] + 1;
-                    }
-                    row++;
+        while(row < meetings.length){
+            if(day >= meetings[row][0]){
+                if(day <= meetings[row][1] || meetings[row][1] > day){
+                    day = meetings[row][1] + 1;
                 }
-                else if(day > meetings[row][1]){
-                  row++;
-                }
-                else if(day <= days){
-                    countD++;
-                    day++;
-                }
-                else{
-                    break;
-                }
+                row++;
             }
-            if(row == meetings.length && day <= days && day > 1){
-              while(day <= days){
-              countD++;
-              day++;
-              }
+            else if(day > meetings[row][1]){
+                row++;
             }
+            else if(day <= days){
+                countD++;
+                day++;
+            }
+            else{
+                break;
+            }
+        }
+        if(row == meetings.length && day <= days && day > 1){
+            while(day <= days){
+            countD++;
+            day++;
+            }
+        }
         return countD;
     }
 }
