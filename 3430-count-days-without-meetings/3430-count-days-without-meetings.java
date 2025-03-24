607afb06d43b1ef06bfde2d92ede0938dbac37a6
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2025-03-24 23:57:13 +0530

    Time: 326 ms (5.05%), Space: 100.4 MB (66.67%) - LeetHub

---
+++ 3430-count-days-without-meetings/3430-count-days-without-meetings.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int countDays(int days, int[][] meetings) {
+        int countD = 0;
+        Arrays.sort(meetings, (a, b) -> Integer.compare(a[0], b[0]));
+        int day = 1;
+        while(day < days){
+            int row = 0;
+            while(row < meetings.length){
+                if(day >= meetings[row][0]){
+                    if(day <= meetings[row][1] || meetings[row][1] > day){
+                      day = meetings[row][1] + 1;
+                    }
+                    row++;
+                }
+                else if(day > meetings[row][1]){
+                  row++;
+                }
+                else if(day <= days){
+                    countD++;
+                    day++;
+                }
+                else{
+                    break;
+                }
+            }
+            if(row == meetings.length && day <= days && day > 1){
+              while(day <= days){
+              countD++;
+              day++;
+              }
+            }
+        }
+        return countD;
+    }
+}
