2f264a33800ab101e7265f2f6f8d9d3d13f2ad82
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2025-03-26 10:03:18 +0530

    Time: 4 ms (13.16%), Space: 41.7 MB (81.62%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,42 +1,40 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> openBracket = new Stack<>();
-        Stack<Character> closedBracket = new Stack<>();
+        Stack<Character> parenthesisStack = new Stack<>();
         for(int index = 0; index < s.length(); index++){
-            if(s.charAt(index) == '(' || s.charAt(index) == '[' || s.charAt(index) == '{'){
-                openBracket.push(s.charAt(index));
+            if(s.charAt(index) == '(' || s.charAt(index) == '{' || s.charAt(index) == '['){
+                parenthesisStack.push(s.charAt(index));
             }
-            else if(s.charAt(index) == ')'){
-                closedBracket.push(s.charAt(index));
-                if(!openBracket.isEmpty()){
-                    if(openBracket.peek() == '('){
-                        openBracket.pop();
-                        closedBracket.pop();
+            if(s.charAt(index) == ')' || s.charAt(index) == '}' || s.charAt(index) == ']'){
+                if(parenthesisStack.isEmpty()){
+                    return false;
+                }
+                else if(s.charAt(index) == ')'){
+                    if(parenthesisStack.peek() == '('){
+                        parenthesisStack.pop();
+                    }
+                    else{
+                        return false;
                     }
                 }
-            }
-            else if(s.charAt(index) == ']'){
-                closedBracket.push(s.charAt(index));
-                if(!openBracket.isEmpty()){
-                    if(openBracket.peek() == '['){
-                        openBracket.pop();
-                        closedBracket.pop();
+                else if(s.charAt(index) == '}'){
+                    if(parenthesisStack.peek() == '{'){
+                        parenthesisStack.pop();
+                    }
+                    else{
+                        return false;
                     }
                 }
-            }
-            else if(s.charAt(index) == '}'){
-                closedBracket.push(s.charAt(index));
-                if(!openBracket.isEmpty()){
-                    if(openBracket.peek() == '{'){
-                        openBracket.pop();
-                        closedBracket.pop();
+                else if(s.charAt(index) == ']'){
+                    if(parenthesisStack.peek() == '['){
+                        parenthesisStack.pop();
+                    }
+                    else{
+                        return false;
                     }
                 }
             }
         }
-        if(openBracket.isEmpty() && closedBracket.isEmpty()){
-            return true;
-        }
-        return false;
+        return parenthesisStack.isEmpty();
     }
 }
