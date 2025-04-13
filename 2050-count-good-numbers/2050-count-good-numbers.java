5956ef0df6d4558121193468f3d245fce9d3e270
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2025-04-13 23:52:29 +0530

    Time: 0 ms (100%), Space: 40.1 MB (98.61%) - LeetHub

--- 2050-count-good-numbers/2050-count-good-numbers.java
+++ 2050-count-good-numbers/2050-count-good-numbers.java
@@ -5,16 +5,16 @@ class Solution {
 
         long result5 = 1;
         long base5 = 5;
-        for(long exponent = evenNumberCountPerDigitExponent; exponent > 0; exponent>>=1){
-            if ((exponent & 1) == 1) {  
+        for(long exponent = evenNumberCountPerDigitExponent; exponent > 0; exponent/=2){
+            if (exponent % 2 == 1) {  
                 result5 = (result5 * base5) % 1000000007;
             }
             base5 = (base5 * base5) % 1000000007;
         }
         long result4 = 1;
         long base4 = 4;
-        for(long exponent = primeNumberCountPerDigitExponent; exponent > 0; exponent>>=1){
-            if ((exponent & 1) == 1) {  
+        for(long exponent = primeNumberCountPerDigitExponent; exponent > 0; exponent/=2){
+            if (exponent % 2 == 1) {  
                 result4 = (result4 * base4) % 1000000007;
             }
             base4 = (base4 * base4) % 1000000007;
