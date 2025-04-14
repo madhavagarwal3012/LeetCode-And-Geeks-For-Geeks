525ad044bf9302569b187cd48fc727af29e3ebd9
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2025-04-14 16:21:53 +0530

    Time: 45 ms (44.59%), Space: 41.2 MB (99.6%) - LeetHub

---
+++ 1488-sort-integers-by-the-power-value/1488-sort-integers-by-the-power-value.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int getKth(int lo, int hi, int k) {
+        int[] powerArray = new int [hi - lo + 1];
+        int[] kArray = new int[hi - lo + 1];
+        int powerArrayIndex = 0;
+        while(lo <= hi){
+            int powerValue = 0;
+            int loTemp = lo;
+            while(loTemp != 1){
+                if(loTemp % 2 == 1){
+                    loTemp = 3 * loTemp + 1;
+                }
+                else{
+                    loTemp /= 2;
+                }
+                powerValue++;
+            }
+            powerArray[powerArrayIndex] = powerValue;
+            kArray[powerArrayIndex] = lo;
+            for(int index = powerArrayIndex; index > 0; index--){
+                if(powerArray[index] < powerArray[index - 1]){
+                    int temp = powerArray[index];
+                    powerArray[index] = powerArray[index - 1];
+                    powerArray[index - 1] = temp;
+
+                    temp = kArray[index];
+                    kArray[index] = kArray[index - 1];
+                    kArray[index - 1] = temp;
+                }
+            }
+            powerArrayIndex++;
+            lo++;
+        }
+        return kArray[k - 1];
+    }
+}
