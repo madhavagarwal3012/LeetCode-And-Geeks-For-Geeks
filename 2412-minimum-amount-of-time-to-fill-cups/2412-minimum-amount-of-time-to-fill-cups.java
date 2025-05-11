a08c035e0c88ae8fd38cd93ce933f51be2bd8b48
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2025-05-11 23:29:17 +0530

    Time: 1 ms (68.44%), Space: 41.5 MB (38%) - LeetHub

---
+++ 2412-minimum-amount-of-time-to-fill-cups/2412-minimum-amount-of-time-to-fill-cups.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int fillCups(int[] amount) {
+        int secondsCount = 0;
+        Arrays.sort(amount);
+        while(amount[0] != 0 || amount[1] != 0 || amount[2] != 0){
+            if((amount[0] == amount[1]) && (amount[1] == amount[2])){
+                secondsCount++;
+                amount[0] = amount[0] - 1;
+                amount[1] = amount[1] - 1;
+            }
+            else if(amount[0] == 0 && amount[1] == 0){
+                secondsCount+=amount[2];
+                amount[2] = 0;
+            }
+            else if(amount[0] == 0 && amount[2] == 0){
+                secondsCount+=amount[1];
+                amount[1] = 0;
+            }
+            else if(amount[2] == 0 && amount[1] == 0){
+                secondsCount+=amount[0];
+                amount[0] = 0;
+            }
+            else if(amount[2] != 0 && amount[1] != 0){
+                secondsCount++;
+                amount[2] = amount[2] - 1;
+                amount[1] = amount[1] - 1;
+            }
+            Arrays.sort(amount);
+        }
+        return secondsCount;
+    }
+}
