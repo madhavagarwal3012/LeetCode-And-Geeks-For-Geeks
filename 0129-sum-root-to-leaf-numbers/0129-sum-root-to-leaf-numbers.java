9c35ece1664d02eab8f3c0fe13d1dd924d2bc4dc
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2025-05-16 03:08:15 +0530

    Time: 4 ms (7.3%), Space: 41.6 MB (14.14%) - LeetHub

---
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void sumNumbersHelper(TreeNode root, ArrayList<String> numberList, StringBuilder number){
+        if(root == null){
+            return;
+        }
+        number.append(String.valueOf(root.val));
+        if (root.left == null && root.right == null) {
+            numberList.add(number.toString());
+        } else {
+            sumNumbersHelper(root.left, numberList, number);
+            sumNumbersHelper(root.right, numberList, number);
+        }
+        number.deleteCharAt(number.length() - 1);
+    }
+    public int sumNumbers(TreeNode root) {
+        ArrayList<String> numberList = new ArrayList<>();
+        StringBuilder number = new StringBuilder("");
+        sumNumbersHelper(root, numberList, number);
+
+        int sum = 0;
+        for(int index = 0; index < numberList.size(); index++){
+            System.out.println(numberList.get(index));
+            sum = sum += Integer.parseInt(numberList.get(index));
+        }
+        return sum;
+    }
+}
