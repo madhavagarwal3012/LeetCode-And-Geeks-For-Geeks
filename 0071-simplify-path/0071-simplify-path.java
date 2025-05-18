74f0d60da6903282766198e7488d5b578d948adb
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2025-05-18 20:45:21 +0530

    Time: 15 ms (5.08%), Space: 47.4 MB (5.27%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.java
@@ -0,0 +1,98 @@
+class Solution {
+    public String simplifyPath(String path) {
+        Stack<Character> pathStack = new Stack<>();
+
+        String check = "";
+        for(int index = 0; index < path.length(); index++){
+            if(path.charAt(index) == '.'){
+                check+=path.charAt(index);
+                if(index == path.length() - 1){
+                    if(check.length() == 2){
+                        if(pathStack.size() > 1){
+                            if(pathStack.peek() != '/'){
+                                pathStack.push('.');
+                                pathStack.push('.');
+                                check = "";
+                                continue;
+                            }
+                            else{
+                                pathStack.pop();
+                            }
+                            while(!pathStack.isEmpty() && pathStack.peek() != '/'){
+                                pathStack.pop();
+                            }
+                        }
+                    }
+                    else if(pathStack.peek() != '/'){
+                        for(int checkCount = 1; checkCount <= check.length(); checkCount++){
+                            pathStack.push('.');
+                        } 
+                    }
+                    else if(check.length() > 2){
+                        for(int checkCount = 1; checkCount <= check.length(); checkCount++){
+                            pathStack.push('.');
+                        }  
+                    }
+                    check = "";
+                }
+            }
+            else if(path.charAt(index) == '/' && check.length() > 0 && check.length() < 3){
+                check+=path.charAt(index);
+                if(check.equals("../")){
+                    if(pathStack.size() > 1){
+                        if(pathStack.peek() == '/'){
+                            pathStack.pop();
+                        }
+                        else{
+                            pathStack.push('.');
+                            pathStack.push('.');
+                            pathStack.push('/');
+                            check = "";
+                            continue;
+                        }
+                    }
+                    while(!pathStack.isEmpty() && pathStack.peek() != '/'){
+                        pathStack.pop();
+                    }
+                    check = "";
+                }
+                else if(pathStack.peek() != '/'){
+                    pathStack.push('.');
+                    pathStack.push('/');
+                    check = "";
+                }
+                else{
+                    check = "";
+                }
+            }
+            else if(check.length() == 1){
+                pathStack.push('.');
+                pathStack.push(path.charAt(index));
+                check = "";
+            }
+            else if(check.length() > 0){
+                for(int checkCount = 1; checkCount <= check.length(); checkCount++){
+                    pathStack.push('.');
+                }
+                check = "";
+                pathStack.push(path.charAt(index));
+            }
+            else{
+                if(path.charAt(index) == '/' && !pathStack.isEmpty() && pathStack.peek() == '/'){
+                    continue;
+                }
+                else{
+                    pathStack.push(path.charAt(index));
+                }
+            }
+        }
+        StringBuilder output = new StringBuilder("");
+        while(pathStack.size() > 1 && pathStack.peek() == '/'){
+            pathStack.pop();
+        }
+        while(!pathStack.isEmpty()){
+            output.append(pathStack.pop());
+        }
+        return output.reverse().toString();
+    }
+}
