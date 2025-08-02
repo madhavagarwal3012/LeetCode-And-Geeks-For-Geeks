57ea93e59eea316987a9a43d77063a10223d9e3d
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2025-08-02 17:21:34 +0530

    Time: 1 ms (82.52%), Space: 42.1 MB (59.8%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -3,27 +3,16 @@ class Solution {
         List<Integer> innerList = new ArrayList<>();
         List<List<Integer>> outerList = new ArrayList<>();
 
-        if(numRows == 1){
-            innerList.add(1);
-            outerList.add(innerList);
-            return outerList;
-        }
-        else if(numRows == 2){
-            innerList.add(1);
-            outerList.add(innerList);
-
-            innerList = new ArrayList<>();
-            innerList.add(1);
-            innerList.add(1);
-
-            outerList.add(innerList);
-            return outerList;
-        }
-        outerList = generate(2);
-        int previousIndex = 1;
-        for(int row = 3; row <= numRows; row++){
+        int previousIndex = -1;
+        for(int row = 1; row <= numRows; row++){
             innerList.add(1);
             int fillerIndex = 1;
+            if(previousIndex < 0){
+                outerList.add(innerList);
+                previousIndex++;
+                innerList = new ArrayList<>();
+                continue;
+            }
             List<Integer> temp = outerList.get(previousIndex);
             for(int elementIndex = 0; elementIndex < temp.size() - 1 && fillerIndex <= numRows - 2; elementIndex++){
                 int sum = temp.get(elementIndex) + temp.get(elementIndex + 1);
