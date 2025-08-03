d87dc38129997376d0e9606644f4ae3a5baa23b9
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2025-08-03 19:57:13 +0530

    Time: 3 ms (53.94%), Space: 44.3 MB (78.8%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -0,0 +1,81 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        // Horizontal Check
+        int row = 0;
+        while(row < 9){
+            HashSet<Character> rowSet = new HashSet<>();
+            int column = 0;
+            while(column < 9){
+                if(board[row][column] == '.'){
+                    column++;
+                    continue;
+                }
+                if(rowSet.contains(board[row][column])){
+                    return false;
+                }
+                rowSet.add(board[row][column]);
+                column++;
+            }
+            row++;
+        }
+
+        //Vertical Check
+        int column = 0;
+        while(column < 9){
+            HashSet<Character> columnSet = new HashSet<>();
+            row = 0;
+            while(row < 9){
+                if(board[row][column] == '.'){
+                    row++;
+                    continue;
+                }
+                if(columnSet.contains(board[row][column])){
+                    return false;
+                }
+                columnSet.add(board[row][column]);
+                row++;
+            }
+            column++;
+        }
+
+        //Block Check;
+        int blockCount = 0;
+        int blockDivision = 0;
+        int steps = 0;
+        int rowPoint = 0;
+        int columnPoint = 0;
+
+        while(blockCount < 9){
+            int startPointR = 0;
+            row = rowPoint;
+            HashSet<Character> blockSet = new HashSet<>();
+            while(startPointR < 3){
+                int startPointC = 0;
+                column = columnPoint;
+                while(startPointC < 3){
+                    if(board[row][column] == '.'){
+                        startPointC++;
+                        column++;
+                        continue;
+                    }
+                    if(blockSet.contains(board[row][column])){
+                        return false;
+                    }
+                    blockSet.add(board[row][column]);
+                    column++;
+                    startPointC++;
+                }
+                row++;
+                startPointR++;
+            }
+            startPointR = 0;
+            blockCount++;
+            columnPoint += 3;
+            if(blockCount % 3 == 0){
+                columnPoint = 0;
+                rowPoint += 3;
+            }
+        }
+        return true;
+    }
+}
