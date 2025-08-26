66c03f6ff57469268b084599cda7fd0308a38a3e
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2025-08-26 23:35:00 +0530

    Time: 1 ms (53.05%), Space: 42.1 MB (27.19%) - LeetHub

--- 2177-check-whether-two-strings-are-almost-equivalent/2177-check-whether-two-strings-are-almost-equivalent.java
+++ 2177-check-whether-two-strings-are-almost-equivalent/2177-check-whether-two-strings-are-almost-equivalent.java
@@ -2,8 +2,8 @@ class Solution {
     public boolean checkAlmostEquivalent(String word1, String word2) {
         int[] frequencyArray = new int[26];
         for(int index = 0; index < word1.length() && index < word2.length(); index++){
-            frequencyArray[word1.charAt(index) - 97]++;
-            frequencyArray[word2.charAt(index) - 97]--;
+            frequencyArray[word1.charAt(index) - 'a']++;
+            frequencyArray[word2.charAt(index) - 'a']--;
         }
         for(int index = 0; index < frequencyArray.length; index++){
             if(Math.abs(frequencyArray[index]) > 3){
