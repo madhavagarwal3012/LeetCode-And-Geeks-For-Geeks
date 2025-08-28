87894813fbbea800813993d5d5b082bcdfc0557f
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2025-08-29 02:07:59 +0530

    Time: 5 ms (98.74%), Space: 42 MB (99.45%) - LeetHub

--- 0179-largest-number/0179-largest-number.java
+++ 0179-largest-number/0179-largest-number.java
@@ -1,24 +1,31 @@
 class Solution {
     public String largestNumber(int[] nums) {
-        String[] elements = new String[nums.length];
-        int index = 0;
+        List<String> list = new ArrayList<>();
         for(int element : nums){
-            elements[index++] = Integer.toString(element);
+            list.add(Integer.toString(element));
         }
-        Arrays.sort(elements, new Comparator<String>(){
-            public int compare(String a, String b){
-                String first = a + b;
-                String second = b + a;
-                return second.compareTo(first);
-            }
-        });
-        if(elements[0].equals("0")){
-            return "0";
+        Collections.sort(list, (a,b) -> compare(a,b));
+        String result = String.join("", list);
+        if(allZeros(result)) return "0";
+        return result;
+    }
+    private boolean allZeros(String number){
+        for(char c : number.toCharArray()){
+            if(c != '0') return false;
         }
-        StringBuilder largestNumber = new StringBuilder();
-        for(String element : elements){
-            largestNumber.append(element);
+        return true;
+    }
+    private int compare(String a, String b){
+        int aIndex = 0;
+        int bIndex = 0;
+        while(aIndex < a.length() || bIndex < b.length()){
+            if(aIndex == a.length()) aIndex = 0;
+            if(bIndex == b.length()) bIndex = 0;
+            if(a.charAt(aIndex) > b.charAt(bIndex)) return -1;
+            if(b.charAt(bIndex) > a.charAt(aIndex)) return 1;
+            aIndex++;
+            bIndex++;
         }
-        return largestNumber.toString();
+        return 0;
     }
 }
