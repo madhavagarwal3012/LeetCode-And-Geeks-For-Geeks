4d31733f6e8bbcf0c8a888e486fe48d9955b9e91
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2025-12-18 21:05:14 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -0,0 +1,40 @@
+class Solution {
+    public static <T> List<T> sortByFrequency(List<T> list, Map<T, Integer> frequencyMap) {
+        Collections.sort(list, new Comparator<T>() {
+            @Override
+            public int compare(T o1, T o2) {
+                
+                int freq1 = frequencyMap.get(o1);
+                int freq2 = frequencyMap.get(o2);
+
+                if (freq1 == freq2) {
+                    return 0;
+                }
+                return Integer.compare(freq2, freq1);
+            }
+        });
+
+        return list;
+    }
+    public int[] topKFrequent(int[] nums, int k) {
+        if (k == 1) return nums;
+        HashMap<Integer, Integer> frequencyMap = new HashMap<>();
+        List<Integer> elements = new ArrayList<>();
+        int[] result = new int[k];
+        
+        for(int index = 0; index < nums.length; index++){
+            if(!frequencyMap.containsKey(nums[index])){
+                frequencyMap.put(nums[index], 1);
+                elements.add(nums[index]);
+            }
+            else{
+                frequencyMap.put(nums[index], frequencyMap.get(nums[index]) + 1);
+            }
+        }
+        elements = sortByFrequency(elements, frequencyMap);
+        for(int index = 0; index < elements.size() && k > 0; index++, k--){
+            result[index] = elements.get(index);
+        }
+        return result;
+    }
+}
